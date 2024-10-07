00d5313628d05a1ba3a1a905c962290924bcded7
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-07 21:21:28 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CouponPrint;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('filament.app.pages.dashboard'));
 });
 
 Route::get('/invoices/{order}/print', [CouponPrint::class, 'print'])->name('invoice.print');
