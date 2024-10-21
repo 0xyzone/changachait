7cb6ebd85ee5067e7b51a547546c744f54998821
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-21 13:06:44 +0545

    added exporter

---
+++ app/Filament/Exports/OrderExporter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Exports;
+
+use App\Models\Order;
+use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
+use Filament\Actions\Exports\Models\Export;
+
+class OrderExporter extends Exporter
+{
+    protected static ?string $model = Order::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ExportColumn::make('id')
+                ->label('ID'),
+            ExportColumn::make('sub_total'),
+            ExportColumn::make('grand_total'),
+            ExportColumn::make('return_amount'),
+            ExportColumn::make('received_amount'),
+            ExportColumn::make('payment_method'),
+            ExportColumn::make('created_at'),
+            ExportColumn::make('updated_at'),
+        ];
+    }
+
+    public static function getCompletedNotificationBody(Export $export): string
+    {
+        $body = 'Your order export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+
+        if ($failedRowsCount = $export->getFailedRowsCount()) {
+            $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
+        }
+
+        return $body;
+    }
+}
